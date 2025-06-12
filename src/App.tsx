06bed28971ade6018ace8d2fd03a10775d09a8e8
rodrigokrapp <rodrigokrapp1@gmail.com>
2025-06-12 20:25:57 -0300

    so jesus
    so jesus

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SalasPage from './pages/SalasPage';
