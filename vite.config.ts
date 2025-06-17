859749ec9b494401c6a9287f3fdd548170a2f4c9
100matchs <rodrigokrapp1@gmail.com>
2025-06-17 01:39:31 -0300

    kjlk
    kljklj

--- vite.config.ts
+++ vite.config.ts
@@ -9,19 +9,31 @@ export default defineConfig({
     host: true
   },
   build: {
+    target: 'esnext',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
-    },
-    target: 'esnext',
-    minify: 'esbuild',
+      external: (id) => {
+        return id.includes('@rollup/rollup-')
+      }
+    }
   },
   optimizeDeps: {
-    exclude: ['@rollup/rollup-linux-x64-gnu'],
+    exclude: [
+      '@rollup/rollup-linux-x64-gnu',
+      '@rollup/rollup-linux-x64-musl',
+      '@rollup/rollup-darwin-x64',
+      '@rollup/rollup-darwin-arm64',
+      '@rollup/rollup-win32-x64-msvc'
+    ],
     include: ['esbuild']
   },
   esbuild: {
     target: 'esnext'
+  },
+  define: {
+    global: 'globalThis',
   }
 }) 
