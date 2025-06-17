ac38e24a15370064d2f1e4b5a4be57bbffbb3074
100matchs <rodrigokrapp1@gmail.com>
2025-06-17 02:45:38 -0300

    Design moderno e ousado - Implementação completa conforme solicitado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,19 +27,34 @@ const Header: React.FC<HeaderProps> = ({ showSupport = true }) => {
     <header className="app-header">
       <div className="header-content">
         <div className="logo-section" onClick={handleLogoClick}>
-          <div className="logo-icon">
-            <svg viewBox="0 0 100 100" className="logo-svg">
-              {/* Balão de fala */}
-              <circle cx="50" cy="40" r="30" fill="#EC4899" stroke="#EC4899" strokeWidth="2"/>
-              
-              {/* Pontinha do balão */}
-              <path d="M35 62 Q40 72 25 68 Z" fill="#EC4899"/>
-              
-              {/* Coração dentro do balão */}
-              <path d="M38 35 C38 32, 41 30, 44 32 C47 30, 50 32, 50 35 C50 38, 44 44, 44 44 C44 44, 38 38, 38 35 Z" fill="white"/>
-              <path d="M50 35 C50 32, 53 30, 56 32 C59 30, 62 32, 62 35 C62 38, 56 44, 56 44 C56 44, 50 38, 50 35 Z" fill="white"/>
-            </svg>
-          </div>
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 100 100"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="logo-icon"
+          >
+            {/* Balão de conversa */}
+            <path
+              d="M75 25C75 15.335 67.165 7.5 57.5 7.5H22.5C12.835 7.5 5 15.335 5 25V50C5 59.665 12.835 67.5 22.5 67.5H35L45 85L55 67.5H57.5C67.165 67.5 75 59.665 75 50V25Z"
+              fill="#E91E63"
+              stroke="#E91E63"
+              strokeWidth="3"
+            />
+            
+            {/* Coração dentro do balão */}
+            <path
+              d="M40 25C37.8 22.8 34.2 22.8 32 25C29.8 22.8 26.2 22.8 24 25C21.8 27.2 21.8 30.8 24 33L32 41L40 33C42.2 30.8 42.2 27.2 40 25Z"
+              fill="white"
+            />
+            
+            {/* Coração adicional menor */}
+            <path
+              d="M60 35C58.5 33.5 56.5 33.5 55 35C53.5 33.5 51.5 33.5 50 35C48.5 36.5 48.5 38.5 50 40L55 45L60 40C61.5 38.5 61.5 36.5 60 35Z"
+              fill="white"
+            />
+          </svg>
           <h1 className="logo-text">100 MATCHS</h1>
         </div>
         
