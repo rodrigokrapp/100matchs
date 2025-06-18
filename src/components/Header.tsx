c41ea983b850b2cab4efd5d2eea54a5b60f4b75e
100matchs <rodrigokrapp1@gmail.com>
2025-06-18 20:51:18 -0300

    ATUALIZAR: Nova logo e fundo chat cor roxa da logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
       <div className="header-left">
         <div className="logo-container" onClick={handleLogoClick}>
           <img 
-            src="/logo-nova-coracao-rosa.svg" 
+            src="/logo-nova-coracao-rosa.png" 
             alt="100 Matchs Logo" 
             className="logo" 
           />
